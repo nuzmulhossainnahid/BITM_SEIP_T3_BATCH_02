59aad474815a084f0eeab53c1321689eed543963
Nuzmul Hossain Nahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2021-11-09 15:39:47 +0600

    Time_Date

--- Day_15/js/main.js
+++ Day_15/js/main.js
@@ -5,13 +5,38 @@ var submit = document.getElementById("submit");
             var i;
             var result = 'prime'
             for(i = 2; i< enNam; i++){
-                if(enNam%i == 0){
+                if(enNam%i == 0)
+                {
                     result = 'Not Prime';
                     break;
                 }
-                else{
+                else
+                {
                     continue;
                 }
             }
-            document.getElementById('result').innerHTML=result;
+            
         }
+        
+        // console.log(watch);
+        // console.log(result);
+        // Time
+        // set interval
+        // set time out
+        function test(){
+            var dateTime = new Date();
+        var month = ['January','February','March','Aprile','May','June','July','August','September','October','November','December'];
+        var day=['Sunday','Monday','Tuesday','Wednesday','Thusday','Friday','Saterday'];
+        var hour = dateTime.getHours();
+        var minute = dateTime.getMinutes();
+        var secound = dateTime.getSeconds();
+        var year = dateTime.getFullYear();
+
+        var result = day[dateTime.getDay()]+','+month[dateTime.getMonth()]+','+year;
+        var watch = hour + ':'+minute +':'+ secound;
+
+        document.getElementById("watch").innerHTML = result +"|"+watch;
+
+        }
+        test();
+        setInterval(test, 1000);
