8d0d89abea9536a4ee06d20143af78189de6f21b
Nuzmul Hossain Nahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2021-11-10 17:18:47 +0600

    Done Day_16

--- Day_16/js/main.js
+++ Day_16/js/main.js
@@ -38,10 +38,44 @@ $('#btn').click(function(){
 
 });
 
+//first to last name
 $('#btnOne').click(function(){
     var firstName = $('#firstName').val();
     var lastName = $('#lastName').val();
 
     var result = firstName+' '+lastName;
     $('#fullName').val(result);
+});
+
+var products = [
+    {id:1, name:'Shirt', price:2000},
+    {id:2, name:'Saee', price:4000},
+    {id:3, name:'T-shirt', price:1200},
+    {id:4, name:'Football', price:1500},
+];
+var select ='';
+select += '<select>';
+
+$.each(products, function(key, value){
+    select +='<option>'+value.name+'</option>';
+});
+
+select += '</select>';
+
+$('#tdOne').append(select);
+
+$('#addBtn').click(function(){
+    var tr ='';
+    tr += '<tr>';
+    tr += '<td>Name</td>';
+    tr += '<td>200</td>';
+    tr += '<td>2</td>';
+    tr += '<td>400</td>';
+    tr += '<td><button type="button" class="remove-btn">-</button></td>';
+    tr +='</tr>';
+    $('#res').append(tr);
+    
+});
+$(document).on('click', '.remove-btn', function(){
+   $(this).closest('tr').remove();
 });
