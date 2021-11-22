5be5fbbeab320e575095aa7616d92cd7dd6f8c28
Nuzmul Hossain Nahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2021-11-22 14:26:56 +0600

    CKEditor

---
+++ jkEditor/js/main.js
@@ -0,0 +1,16 @@
+ $(document).ready(function() {
+        var owl = $('.owl-carousel');
+        owl.owlCarousel({
+          items: 4,
+          loop: true,
+          margin: 10,
+          autoplay: true,
+          autoplayTimeout: 1000,
+          autoplayHoverPause: true
+        });
+
+        $('#example').DataTable();
+        $('.selectTwo').select2();
+
+        $('#editor').
+    })
